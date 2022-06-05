344d77fed870f4ca36f777e0d3b5667e767a20d8
Joshua@4221 <joshuaejike4221@gmail.com>
2022-06-05 20:08:14 +0100

    fixing dashboard error

--- components/Channel Post/index.js
+++ components/Channel Post/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { withTheme } from "styled-components";
 import { BodyDiv } from "../../universal-components/body";
 import Nav from "../../universal-components/Nav";
 import NavArrayDashboard from "../../util/New-User-Select-Channel/Nav";
 
-const ChannelPost = () => {
+const ChannelPost = ({ theme: { Color } }) => {
   return (
-    <BodyDiv>
+    <BodyDiv Color={Color}>
       <Nav NavArrayContent={NavArrayDashboard} />
+      <div className={"body"}></div>
     </BodyDiv>
   );
 };
 
-export default ChannelPost;
+export default withTheme(ChannelPost);
 BodyDiv;
---
+++ pages/dashboard/channelpost.js
@@ -0,0 +1,11 @@
+import ChannelPost from "../../components/Channel Post";
+
+const channelpost = () => {
+  return (
+    <div>
+      <ChannelPost />
+    </div>
+  );
+};
+
+export default channelpost;
