834d7fb864e8723b211693346a81b230c47c3069
sixtusiwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-06-15 12:33:54 +0100

    adding the editor component

--- universal-components/Create Channel/index.js
+++ universal-components/Create Channel/index.js
@@ -2,9 +2,13 @@ import { CreateChannelArray } from "../../util/Create Channel";
 import Form from "../Form";
 
 const ChannelCreater = () => {
+
+
+
   return (
     <div>
       <Form FormArray={CreateChannelArray} width={"65%"} />
+     
     </div>
   );
 };
